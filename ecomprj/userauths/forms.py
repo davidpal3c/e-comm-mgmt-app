a0a3a2fbb29ddfbda31ba19fff6a16fc295c10de
David Palacios <davidpal3c@gmail.com>
2024-03-05 23:45:22 -0700

    sign-up template

--- ecomprj/userauths/forms.py
+++ ecomprj/userauths/forms.py
@@ -1,9 +1,11 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from userauths.models import User
+from django.forms import TextInput
 
 class UserRegisterForm(UserCreationForm):
-    
+    username = forms.CharField(widget=TextInput(attrs={"placeholder": "Username"}))
+
     class Meta:             
         model = User        # model userReg will be interacting with (abstractUser in /admin app)
         fields = ['username', 'email']         # passes the field that are going to show up
